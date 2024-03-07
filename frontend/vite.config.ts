f6bc3297674c6cb3981b35436990b42651869786
rida <rida@rakhsas.rakhsas>
2024-03-07 20:50:52 +0100

    asdf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   // server: {
-  //   // port: 4200,
-  //   // host: '0.0.0.0',
-  //   // hmr: {
-  //   //   host: "10.14.58.42",
-  //   //   protocol: "ws",
-  //   // },
-  // },
+    //   // host: '0.0.0.0',
+    //   // hmr: {
+      //   //   host: "10.14.58.42",
+      //   //   protocol: "ws",
+      //   // },
+      // },
+      server: {
+        port: 4200,
+        host: '0.0.0.0'
+  },
   plugins: [react()],
 })
