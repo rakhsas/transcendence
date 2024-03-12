e24e06246b2945a1ed7678f4b0549771e0ab1327
rida <rida@rakhsas.rakhsas>
2024-03-12 16:02:48 +0000

    to select users friends

--- auth/src/friends/friends.controller.ts
+++ auth/src/friends/friends.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, Post } from "@nestjs/common";
 import { FriendService } from "./friends.service";
 import { UUID } from "crypto";
+import { ApiParam } from "@nestjs/swagger";
 
 @Controller('friends')
 export class friendController {
@@ -8,8 +9,18 @@ export class friendController {
     constructor(private readonly friendService: FriendService) {}
 
     @Get(':userId')
-    async getFriendsOfUser(@Param('userId') userId: UUID)
+    @ApiParam({ name: 'userId', description: 'ID of the user' })
+    async getFriendsOfUser(@Param('userId')userId: string,)
     {
         return this.friendService.getFriends(userId);
+    }
+    
+    @Post(':userId/:friendId')
+    @ApiParam({ name: 'userId', description: 'ID of the user' })
+    @ApiParam({ name: 'friendId', description: 'ID of the user' })
+    async creeateFriendship(@Param('userId')userId: string, @Param('friendId')friendId: string,)
+    {
+        this.friendService.createFriendship(friendId, userId);
+        return this.friendService.createFriendship(userId, friendId);
     }
 }
--- auth/src/friends/friends.module.ts
+++ auth/src/friends/friends.module.ts
@@ -4,13 +4,19 @@ import { FriendService } from "./friends.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Friendship } from "src/user/entities/freindship.entity";
 import { User } from "src/user/entities/user.entity";
+import { Repository } from "typeorm";
+import { UserService } from "src/user/user.service";
+import { HttpModule, HttpService } from "@nestjs/axios";
+import { UserModule } from "src/user/user.module";
 
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Friendship]),
+    TypeOrmModule.forFeature([Friendship, User]),
+    HttpModule,
+    UserModule
   ],
-    providers: [FriendService],
+    providers: [FriendService, Repository, FriendService, UserService],
     controllers: [friendController],
   })
   export class FreindsModule {}
