86117f1b6d23f6e406c0794d29809d1423f6164b
rida <rida@rakhsas.rakhsas>
2024-03-15 04:27:37 +0000

    asdf3

--- auth/src/auth/auth.controller.ts
+++ auth/src/auth/auth.controller.ts
@@ -27,7 +27,7 @@ export class AuthController {
         const accessToken = req.user.appAccessToken;
         const providerAccessToken = req.user.providerAccessToken;
         res.cookie('access_token', accessToken, { httpOnly: true});
-        res.cookie('provider_access_token', providerAccessToken, { sameSite: 'strict'});
+        res.cookie('provider_access_token', providerAccessToken, { httpOnly: true});
         res.cookie('isAuthenticated', true);
         res.cookie('firstLogin', firstLogin);
         // return {
