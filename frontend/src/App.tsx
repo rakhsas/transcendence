1307430001419b6105ff6a4f69e12862b273908d
rida <rida@rakhsas.rakhsas>
2024-04-03 03:28:23 +0000

    asdf

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,18 @@
 
 // import './list.css';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePageComponent from './Components/HomePage';
-import ValidInformation from './Components/Info/Information';
 import DashboardComponent from './Components/dashboard/Dashboard';
 import AnalyticsComponent from './Components/main/analytics/analytics';
 import HomeComponent from './Components/main/home/Home'
 import Game from './Components/Game/Game.tsx';
 import HeadToHead from './Components/Game/headToHead.tsx';
-import Cookies from 'js-cookie';
 import chatComponent from './Components/main/chat/chat';
 import CallComponent from './Components/call/call';
-import SettingsComponent from './Components/main/settings/settings.tsx';
 import ProfileComponent from './Components/main/profile/profile.tsx';
 import NotFoundComponent from './Components/error/FourHunFour.tsx';
-import { useEffect, useState } from 'react';
-import INVALIDQRCODEComponent from './Components/error/InvalidQrcode.tsx';
 import SettingFunction from './Components/main/settings/settings.tsx';
+import FunctionProfileForm from './Components/main/profile/profile.tsx';
 // const url: string = import.meta.env.VITE_API_AUTH_KEY;
 
 function App() {
@@ -35,7 +31,7 @@ function App() {
           <Route path="profile/*" element={<ProfileComponent />} />
           <Route path="game" element={<Game />} />
           <Route path="HeadToHead" element={<HeadToHead />} />
-          <Route path="profile" element={<ProfileComponent />} />
+          <Route path="profile" element={<FunctionProfileForm />} />
           <Route path="*" element={<NotFoundComponent />} />
         </Route>
       </Routes>
