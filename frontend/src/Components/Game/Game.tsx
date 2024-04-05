de7f67502b109ba26caa37b173d39b7d56ae3d9d
hamza benfaddoul <benfaddoul01@gmail.com>
2024-04-05 16:23:25 +0000

    update game with robo

--- frontend/src/Components/Game/Game.tsx
+++ frontend/src/Components/Game/Game.tsx
@@ -2,7 +2,7 @@ import Canvas from './Canvas/Canvas'
 
 const Game = () => {
   return (
-    <div className='h-lvh flex justify-center items-center'>
+      <div className="flex m-auto  border-2 border-red-600 rounded-xl">
       <Canvas
         width="600"
         height="400"
